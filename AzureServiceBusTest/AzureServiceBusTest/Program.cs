2b2f7ffd6ea816a7eda2d8e4012f26faf3f45662
MaiQD <q.dat2710@gmail.com>
2022-05-22 19:38:07 +0700

    service bus integrate with azure function

---
+++ AzureServiceBusTest/AzureServiceBusTest/Program.cs
@@ -0,0 +1,21 @@
+﻿//connection string to your service bus namespace
+using Azure.Messaging.ServiceBus;
+using System.Text;
+
+var connectionString = "Endpoint=sb://datmq-servicebus-namespace.servicebus.windows.net/;SharedAccessKeyName=ReadWrite;SharedAccessKey=tKJXlvwCDGrsK+t9VbGOMarc+ztwNWD5p2QvZVP7UHE=;";
+
+var queueName = "servicebus-test";
+
+ServiceBusClient client = new ServiceBusClient(connectionString);
+ServiceBusSender sender = client.CreateSender(queueName);
+
+Console.WriteLine("Enter your message:");
+var message = Console.ReadLine();
+
+//for loop 0 - 10
+for (int i = 0; i < 10; i++)
+{
+	var sentMessage = new ServiceBusMessage(Encoding.UTF8.GetBytes($"{message} - {i}"));
+	// Send the message to the queue
+	await sender.SendMessageAsync(sentMessage);
+}
